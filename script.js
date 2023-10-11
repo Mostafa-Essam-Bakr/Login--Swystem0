4f5d85d6b7fb4f5f01c10e4c81ebc657ca8e407c
Mostafa-Essam-Bakr <145873246+Mostafa-Essam-Bakr@users.noreply.github.com>
2023-10-11 19:06:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+window.alert("welcome we are happy to meet you"); /*رساله اول ماتفتح الموقع*/
+
+window.prompt("please enter yor name ");   /*ادخال الاسم اول ماتفتح الموقع*/
+
+
+
+
+function print()
+{
+    document.getElementById("t1").innerHTML="yuo are here now";
+}
+
+
+
+
+
+
+function print()
+{
+    document.getElementById("o1").innerHTML="<li>profile</li> <li>images</li> <li>serces</li> <li>activetes</li>  <li>praivasy</li>";
+}
+
+
+
+var n=document.getElementById("in1").value;
+
+
+
+document.getElementById("ab").onclick = function(){
+    document.write(`<h1> helooooooo my frindes how can i help you</h1>`);
+}
